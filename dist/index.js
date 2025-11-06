6ba74f5700901943cd9f3a4d0c3172c2b2d1391a
Bjørn Wiegell <bjwi@trifork.com>
2025-11-06 08:47:52 +0100

    cleanup

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-import { GetHasIdDocument } from './generated/graphql';
-const operationDefinition = GetHasIdDocument.definitions.find((definition) => definition.kind === 'OperationDefinition');
-console.log('GetHasId operation:', operationDefinition?.name?.value ?? 'unknown');
-console.log('Result type example:', null);
--- node_modules/@graphql-codegen/cli/typings/codegen.d.cts
+++
@@ -1,6 +0,0 @@
-import { Types } from '@graphql-codegen/plugin-helpers';
-import { CodegenContext } from './config.cjs';
-export declare function executeCodegen(input: CodegenContext | Types.Config): Promise<{
-    result: Types.FileOutput[];
-    error: Error | null;
-}>;
