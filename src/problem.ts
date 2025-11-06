36a7652c6a938230044837a4ccec3f51c4243fd7
Bjørn Wiegell <bjwi@trifork.com>
2025-11-06 08:49:14 +0100

    cleanup

--- src/problem.ts
+++ src/problem.ts
@@ -1,14 +1,5 @@
 import { GetHasIdQuery, HasId } from "./generated/graphql";
 
-export const sampleResult: GetHasIdQuery = {
-  __typename: "Query",
-  character: {
-    __typename: "Droid",
-    id: "R2-D2",
-    primaryFunction: "Astromech",
-  },
-};
-
 export function someFunc(result: GetHasIdQuery) {
   const tmp: HasId = result;
 }
